553d624dddc4609be1e739cc3f6910507b752be6
Buzhak <Byzhak@ya.ru>
2023-09-13 22:07:44 +0300

    add dotenv

--- src/core/settings.py
+++ src/core/settings.py
@@ -1,10 +1,15 @@
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-SECRET_KEY = 'django-insecure-&@jyyt-zi283urxz6+2^a+f0x!qb#69^zbe=*n4mblnm0-q2s6'
+SECRET_KEY = os.getenv('SECRET_KEY', default='django-insecure-&@jyyt-zi283urxz6+2^a+f0x!qb#69^zbe=*n4mblnm0-q2s6')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN', default=None)
 
 DEBUG = True
 
