914b362c48b936f6f5a19997d3d247d8f774c8b1
Maxim Isaev <isaev_0602@mail.ru>
2023-09-14 04:13:19 +0500

    Прописаны модели, настроена админка, созданы миграции.

--- src/bot/admin.py
+++ src/bot/admin.py
@@ -1,23 +1,71 @@
 from django.contrib import admin
 
-from .models import CreatedPair, PassedPair, Recruter, Student
+from .models import CreatedPair, PassedPair, Recruiter, Student
 
 
 @admin.register(CreatedPair)
 class CreatedPairAdmin(admin.ModelAdmin):
     """Управление созданной парой."""
 
+    list_display = ("id", "student", "recruiter", "date")
+    list_filter = ("date",)
+    search_fields = (
+        "student__user_id",
+        "recruiter__user_id",
+        "student__tg_username",
+        "recruiter__tg_username",
+    )
+
 
 @admin.register(PassedPair)
 class PassedPairAdmin(admin.ModelAdmin):
     """Управление моделью созвона."""
+
+    list_display = (
+        "id",
+        "student",
+        "recruiter",
+        "date",
+        "is_interview_successful",
+    )
+    list_filter = ("date", "is_interview_successful")
+    search_fields = (
+        "student__user_id",
+        "recruiter__user_id",
+        "student__tg_username",
+        "recruiter__tg_username",
+    )
 
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     """Управление моделью студента."""
 
+    list_display = (
+        "user_id",
+        "name",
+        "surname",
+        "tg_username",
+        "registration_date",
+        "last_login_date",
+        "is_vacant",
+    )
+    list_filter = ("registration_date", "last_login_date", "is_vacant")
+    search_fields = ("user_id", "tg_username")
 
-@admin.register(Recruter)
+
+@admin.register(Recruiter)
 class RecruterAdmin(admin.ModelAdmin):
     """Управление моделью рекрутера."""
+
+    list_display = (
+        "user_id",
+        "name",
+        "surname",
+        "tg_username",
+        "registration_date",
+        "last_login_date",
+        "is_vacant",
+    )
+    list_filter = ("registration_date", "last_login_date", "is_vacant")
+    search_fields = ("user_id", "tg_username")
--- src/bot/models.py
+++ src/bot/models.py
@@ -1,25 +1,112 @@
 from django.db import models
 
 
+class ProfessionChoice(models.TextChoices):
+    """Класс для выбора профессий."""
+
+    ANALYST = "AN", "Аналитик"
+    BACKEND = "BA", "Бэкенд-разработчик"
+    FRONTEND = "FR", "Фронтенд-разработчик"
+    TESTER = "TE", "Тестировщик"
+
+
 class PracticumUser(models.Model):
     """Базовая модель для пользователей."""
+
+    user_id = models.IntegerField(primary_key=True, verbose_name="Telegram id")
+    name = models.CharField(max_length=255, verbose_name="Имя")
+    surname = models.CharField(max_length=255, verbose_name="Фамилия")
+    tg_username = models.CharField(
+        max_length=255, verbose_name="Ник в телеграмме"
+    )
+    registration_date = models.DateField(
+        auto_now_add=True, verbose_name="Дата регистрации"
+    )
+    last_login_date = models.DateField(
+        auto_now=True, verbose_name="Заходил в последний раз"
+    )
+    is_vacant = models.BooleanField(default=False, verbose_name="Есть пара")
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return f"{self.tg_username} | id: {self.user_id}"
 
 
 class Student(PracticumUser):
     """Модель для студентов."""
 
+    profession = models.CharField(
+        max_length=2,
+        choices=ProfessionChoice.choices,
+        default=ProfessionChoice.ANALYST,
+        verbose_name="Профессия",
+    )
 
-class Recruter(PracticumUser):
+    class Meta:
+        verbose_name = "Студент"
+        verbose_name_plural = "Студенты"
+
+
+class Recruiter(PracticumUser):
     """Модель для рекрутеров."""
+
+    class Meta:
+        verbose_name = "Рекрутер"
+        verbose_name_plural = "Рекрутеры"
 
 
 class CustomPair(models.Model):
     """Базовая модель для создания пар."""
 
+    student = models.ForeignKey(
+        Student, on_delete=models.CASCADE, verbose_name="Студент"
+    )
+    recruiter = models.ForeignKey(
+        Recruiter, on_delete=models.CASCADE, verbose_name="Рекрутер"
+    )
+    date = models.DateField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return (
+            f"Студент {self.student.tg_username} | "
+            f"Рекрутер {self.recruiter.tg_username}"
+        )
+
 
 class CreatedPair(CustomPair):
     """Модель для создания пары."""
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["student"], name="unique_student"),
+            models.UniqueConstraint(
+                fields=["recruiter"], name="unique_recruiter"
+            ),
+            models.UniqueConstraint(
+                fields=["student", "recruiter"], name="unique_created_pair"
+            ),
+        ]
+        verbose_name = "Текущая пара"
+        verbose_name_plural = "Текущие пары"
+
 
 class PassedPair(CustomPair):
     """Модель созвона."""
+
+    is_interview_successful = models.BooleanField(
+        default=False, verbose_name="Встреча прошла успешно"
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["student", "recruiter"], name="unique_passed_pair"
+            )
+        ]
+        verbose_name = "Завершенная пара"
+        verbose_name_plural = "Завершенные пары"
