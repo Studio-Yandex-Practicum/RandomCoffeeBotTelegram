174a64d8ee90ceb4c0fb263b1816ff0c40387beb
Isaev <isaev_0602@mail.ru>
2023-09-18 16:25:26 +0500

    Добавлен обработчик для команды start согласно схеме.

---
+++ src/bot/keyboards/command_keyboards.py
@@ -0,0 +1,14 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+
+from bot.constants.buttons import GO_BUTTON, NEXT_TIME_BUTTON
+
+start_keyboard_markup = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(text=GO_BUTTON, callback_data="go"),
+            InlineKeyboardButton(
+                text=NEXT_TIME_BUTTON, callback_data="next_time"
+            ),
+        ]
+    ]
+)
