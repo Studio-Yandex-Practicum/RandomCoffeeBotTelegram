9d778c08c8c8b54b6c6134d671fcffa15a73490f
Антон Сободаж <asobodazh@mail.ru>
2023-09-18 19:53:18 +0300

    fix

--- src/bot/constants/buttons.py
+++ src/bot/constants/buttons.py
@@ -1,4 +1,4 @@
-GO_BUTTON = "GO"
-NEXT_TIME_BUTTON = "В следующий раз"
 PARTICIPATE_BUTTON = "Участвовать!"
 SUPPORT_BUTTON = "Написать в поддержку"
+GO_BUTTON = "GO"
+NEXT_TIME_BUTTON = "В следующий раз"
