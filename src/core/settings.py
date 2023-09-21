64b44994035079766e8f2bfbca007634a9321ad7
Buzhak <Byzhak@ya.ru>
2023-09-21 21:08:00 +0300

    fix tests

--- src/core/settings.py
+++ src/core/settings.py
@@ -65,12 +65,12 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": env.str("POSTGRES_ENGINE"),
-        "NAME": env.str("POSTGRES_NAME"),
-        "USER": env.str("POSTGRES_USER"),
-        "PASSWORD": env.str("POSTGRES_PASSWORD"),
-        "HOST": env.str("POSTGRES_HOST"),
-        "PORT": env.str("POSTGRES_PORT"),
+        "ENGINE": env.str("POSTGRES_ENGINE", default='django.db.backends.postgresql'),
+        "NAME": env.str("POSTGRES_NAME", default='postgres'),
+        "USER": env.str("POSTGRES_USER", default='postgres'),
+        "PASSWORD": env.str("POSTGRES_PASSWORD", default='postgres'),
+        "HOST": env.str("POSTGRES_HOST", default='db'),
+        "PORT": env.str("POSTGRES_PORT", default='5432'),
     }
 }
 
