62cc0493d96c6eeeadff0204573a5b062e5731d1
Klemix <53221190+KlemixSurfer@users.noreply.github.com>
2023-09-30 22:47:02 +0300

    added menu button (#86)
    * added menu button
    
    * fixed format code
    
    * fixed format code
    
    * fixed format code
    
    * fixed format code
    
    * fix ruff
    
    ---------
    
    Co-authored-by: Konstantin Raikhert <raikhert13@gmail.com>

---
+++ src/bot/constants/commands.py
@@ -0,0 +1,6 @@
+START_COMMAND = "start"
+HELP_COMMAND = "help"
+SUPPORT_COMMAND = "support"
+START_DESCRIPTION = "Запустить бота"
+HELP_DESCRIPTION = "Получить помощь"
+SUPPORT_DESCRIPTION = "Связаться с поддержкой"
