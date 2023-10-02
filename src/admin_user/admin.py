fc72cd6b825bbbdd711da0248976181b9eaf8342
Михаил Отрашкевич <Klemixsurfer@gmail.com>
2023-10-02 18:09:14 +0300

    deleted groups from admin and change title and header

--- src/admin_user/admin.py
+++ src/admin_user/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
+
 
 AdminUser = get_user_model()
+
+admin.site.unregister(Group)
+admin.site.site_header = "Администрирование RandomCoffeeBot"
+admin.site.index_title = "Панель администратора бота"
 
 
 @admin.register(AdminUser)
