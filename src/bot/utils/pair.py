6399c415126d42eee94b042a07396aa22d4c6679
n-fdrv <waca322@yandex.ru>
2023-10-11 10:25:15 +0300

    func make_pair now return bool and added update_fields to asave()

--- src/bot/utils/pair.py
+++ src/bot/utils/pair.py
@@ -1,31 +1,29 @@
 from django.db import IntegrityError
 from loguru import logger
 
-from bot.constants.states import States
 from bot.models import CreatedPair, Recruiter, Student
 
 
-async def make_pair(student: Student, recruiter: Recruiter) -> States:
+async def make_pair(student: Student, recruiter: Recruiter) -> bool:
     """Функция для создания пары студент-рекрутер."""
+    status = False
     try:
         student.has_pair = True
         recruiter.has_pair = True
         created_pair = await CreatedPair.objects.acreate(
             student=student, recruiter=recruiter
         )
-        await student.asave()
-        await recruiter.asave()
-        state = States.PAIR_FOUND
+        await student.asave(update_fields=["has_pair"])
+        await recruiter.asave(update_fields=["has_pair"])
         logger.info(f"The pair was made with {created_pair}")
+        status = True
     except IntegrityError as error:
-        state = States.PAIR_NOT_FOUND
         logger.error(f"Error in creating objects in database: {error}")
     except Exception as exp:
-        state = States.PAIR_NOT_FOUND
         logger.error(
             (
                 f"Error in making pair with {student.name} "
                 f"and {recruiter.name}: {exp}"
             )
         )
-    return state
+    return status
