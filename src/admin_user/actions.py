bfbe0c27516b0027f0cb66eb4dc142b6cd759c56
n-fdrv <waca322@yandex.ru>
2023-10-25 19:39:24 +0300

    add cases constant

--- src/admin_user/actions.py
+++ src/admin_user/actions.py
@@ -3,7 +3,7 @@ import asyncio
 from django.apps import apps
 from django.contrib import admin
 
-from bot.models import Student
+from bot.constants.cases import PARENT_CASE_ROLE
 from bot.utils.message_senders import send_deleting_from_db_message
 
 
@@ -14,15 +14,12 @@ def delete_users_and_send_message(modeladmin, request, queryset):
     app_config = apps.get_app_config("bot")
     bot = app_config.bot
     job_queue = asyncio.run(bot.get_job_queue())
-    role = "Рекрутера"
-    if queryset.model == Student:
-        role = "Студента"
     for user in users:
         job_queue.run_once(
             callback=send_deleting_from_db_message,
             when=0,
             user_id=user["telegram_id"],
             name=user["telegram_username"],
-            data=role,
+            data=PARENT_CASE_ROLE[queryset.model],
         )
     queryset.delete()
