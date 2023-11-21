bfcd5c8ed297dfbc217a72549b20e757c13ed888
Iaroslav Andreev <1frost34eone@gmail.com>
2023-11-21 18:26:23 +0300

    done refactoring (#193)
    * done refactoring
    
    * fixed
    
    * added Literal

--- src/bot/handlers/conversation_handlers.py
+++ src/bot/handlers/conversation_handlers.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Literal, Optional, Union
 
 from django.utils import timezone
 from loguru import logger
@@ -48,205 +48,260 @@ TIME_IN_SECONDS = (
 
 
 @debug_logger
-async def go(update: Update, context: CallbackContext):
+async def go(update: Update, context: CallbackContext) -> Optional[States]:
     """Обработчик кнопки "GO"."""
-    user = update.callback_query.from_user
     query = update.callback_query
-    await query.answer()
-    await query.edit_message_reply_markup(reply_markup=None)
-    if not await user_is_exist(user.id):
-        await query.edit_message_text(CHOOSE_ROLE_MESSAGE)
-        await query.edit_message_reply_markup(role_choice_keyboard_markup)
-        return States.ROLE_CHOICE
-    else:
-        return await search_pair(update, context)
+    if query:
+        user = query.from_user
+        await query.answer()
+        await query.edit_message_reply_markup(reply_markup=None)
+        if not await user_is_exist(user.id):
+            await query.edit_message_text(CHOOSE_ROLE_MESSAGE)
+            await query.edit_message_reply_markup(role_choice_keyboard_markup)
+            return States.ROLE_CHOICE
+        else:
+            return await search_pair(update, context)
+    return None
 
 
-async def search_pair(update: Update, context: CallbackContext):
+async def search_pair(
+    update: Update, context: CallbackContext
+) -> Literal[States.CALLING_IS_SUCCESSFUL]:
     """Поиск пары."""
     query = update.callback_query
-    role = context.user_data["role"]
-    telegram_id = query.from_user.id
-    model, opposite_model = (
-        (Student, Recruiter) if role == "student" else (Recruiter, Student)
-    )
-    current_user = await model.objects.aget(telegram_id=telegram_id)
-    current_user.search_start_time = timezone.now()
-    await current_user.asave(update_fields=("search_start_time",))
-    found_user = (
-        await opposite_model.objects.filter(has_pair=False)
-        .exclude(**{f"passedpair__{role}": telegram_id})
-        .order_by("search_start_time")
-        .afirst()
-    )
-    if found_user:
-        return await found_pair(update, context, current_user, found_user)
-    await query.message.reply_text(PAIR_SEARCH_MESSAGE)
+    if query and context.user_data and query.message:
+        role = context.user_data["role"]
+        telegram_id = query.from_user.id
+        model, opposite_model = (
+            (Student, Recruiter) if role == "student" else (Recruiter, Student)
+        )
+        current_user = await model.objects.aget(telegram_id=telegram_id)
+        current_user.search_start_time = timezone.now()
+        await current_user.asave(update_fields=("search_start_time",))
+        found_user = (
+            await opposite_model.objects.filter(has_pair=False)
+            .exclude(**{f"passedpair__{role}": telegram_id})
+            .order_by("search_start_time")
+            .afirst()
+        )
+        if found_user:
+            return await found_pair(update, context, current_user, found_user)
+        await query.message.reply_text(PAIR_SEARCH_MESSAGE)
     return States.CALLING_IS_SUCCESSFUL
 
 
 @debug_logger
 async def found_pair(
-    update: Update, context: CallbackContext, current_user, found_user
-):
+    update: Update,
+    context: CallbackContext,
+    current_user: Union[Recruiter, Student],
+    found_user: Union[Recruiter, Student],
+) -> Literal[States.CALLING_IS_SUCCESSFUL]:
     """Обработчик найденной пары found_pair."""
-    student, recruiter = (
-        (current_user, found_user)
-        if context.user_data["role"] == "student"
-        else (found_user, current_user)
-    )
-    if await make_pair(student, recruiter):
-        context.job_queue.run_once(
-            callback=send_is_pair_successful_message,
-            when=TIME_IN_SECONDS,
-            user_id=student.telegram_id,
+    if context.user_data and context.job_queue:
+        student, recruiter = (
+            (current_user, found_user)
+            if context.user_data["role"] == "student"
+            else (found_user, current_user)
         )
-        context.job_queue.run_once(
-            callback=send_is_pair_successful_message,
-            when=TIME_IN_SECONDS,
-            user_id=recruiter.telegram_id,
-        )
-        await send_both_users_message(update, context, student, recruiter)
+        if await make_pair(student, recruiter):
+            context.job_queue.run_once(  # type: ignore[attr-defined]
+                callback=send_is_pair_successful_message,
+                when=TIME_IN_SECONDS,
+                user_id=student.telegram_id,
+            )
+            context.job_queue.run_once(  # type: ignore[attr-defined]
+                callback=send_is_pair_successful_message,
+                when=TIME_IN_SECONDS,
+                user_id=recruiter.telegram_id,
+            )
+            await send_both_users_message(update, context, student, recruiter)
     return States.CALLING_IS_SUCCESSFUL
 
 
 @debug_logger
-async def next_time(update: Update, context: CallbackContext):
+async def next_time(
+    update: Update, context: CallbackContext
+) -> Literal[States.NEXT_TIME]:
     """Обработчик кнопки "В следующий раз"."""
     query = update.callback_query
-    await query.answer()
-    await query.edit_message_text(NEXT_TIME_MESSAGE)
-    await query.edit_message_reply_markup(reply_markup=restart_keyboard_markup)
+    if query:
+        await query.answer()
+        await query.edit_message_text(NEXT_TIME_MESSAGE)
+        await query.edit_message_reply_markup(
+            reply_markup=restart_keyboard_markup
+        )
     return States.NEXT_TIME
 
 
 @debug_logger
-async def restart_callback(update: Update, context: CallbackContext):
+async def restart_callback(
+    update: Update, context: CallbackContext
+) -> Literal[States.START]:
     """Обработчик для кнопки start."""
     query = update.callback_query
-    await query.answer()
+    if query:
+        await query.answer()
     return await start(update, context)
 
 
 @debug_logger
-async def role_choice(update: Update, context: CallbackContext):
+async def role_choice(
+    update: Update, context: CallbackContext
+) -> Literal[States.SET_NAME]:
     """Обработчик для выбора роли."""
     query = update.callback_query
-    context.user_data["role"] = query.data
-    await send_name_message(update, context)
+    if query and context.user_data:
+        context.user_data["role"] = query.data
+        await send_name_message(update, context)
     return States.SET_NAME
 
 
 @debug_logger
-async def change_name(update: Update, context: CallbackContext):
+async def change_name(
+    update: Update, context: CallbackContext
+) -> Literal[States.SET_NEW_NAME]:
     """Обработчик для кнопки "Изменить имя"."""
     query = update.callback_query
-    await query.answer()
-    await query.edit_message_text(CHANGE_NAME_MESSAGE)
+    if query:
+        await query.answer()
+        await query.edit_message_text(CHANGE_NAME_MESSAGE)
     return States.SET_NEW_NAME
 
 
 @debug_logger
-async def set_new_name(update: Update, context: CallbackContext):
+async def set_new_name(
+    update: Update, context: CallbackContext
+) -> Literal[States.SET_NAME]:
     """Обработчик для ввода нового имени."""
-    new_name = update.message.text
-    context.user_data["name"] = new_name
-    await send_name_message(update, context)
+    if context.user_data and update.message:
+        context.user_data["name"] = update.message.text
+        await send_name_message(update, context)
     return States.SET_NAME
 
 
 @debug_logger
-async def continue_name(update: Update, context: CallbackContext):
+async def continue_name(
+    update: Update, context: CallbackContext
+) -> Optional[
+    Literal[States.PROFESSION_CHOICE, States.PROFILE, States.SET_PHONE_NUMBER]
+]:
     """Обработчик для кнопки 'Продолжить'."""
     query = update.callback_query
-    if context.user_data["role"] == "student":
-        page_number = parse_callback_data(query.data)
-        await query.answer()
-        keyboard = await build_profession_keyboard(page_number)
-        if query.message.reply_markup.to_json() != keyboard.markup:
-            await query.edit_message_text(CHOOSE_PROFESSION_MESSAGE)
-            await query.edit_message_reply_markup(reply_markup=keyboard.markup)
-        return States.PROFESSION_CHOICE
-    else:
-        context.user_data["profession"] = "It-рекрутер"
-        return await check_username(update, context)
+    if (
+        query
+        and context.user_data
+        and query.message
+        and query.data
+        and query.message.reply_markup
+    ):
+        if context.user_data["role"] == "student":
+            page_number = parse_callback_data(query.data)
+            await query.answer()
+            keyboard = await build_profession_keyboard(page_number)
+            if query.message.reply_markup.to_json() != keyboard.markup:
+                await query.edit_message_text(CHOOSE_PROFESSION_MESSAGE)
+                await query.edit_message_reply_markup(
+                    reply_markup=keyboard.markup
+                )
+            return States.PROFESSION_CHOICE
+        else:
+            context.user_data["profession"] = "It-рекрутер"
+            return await check_username(update, context)
+    return None
 
 
 @debug_logger
-async def profession_choice(update: Update, context: CallbackContext):
+async def profession_choice(
+    update: Update, context: CallbackContext
+) -> Optional[Literal[States.PROFILE, States.SET_PHONE_NUMBER]]:
     """Обработчик для выбора профессии."""
     query = update.callback_query
-    profession = await Profession.objects.aget(professional_key=query.data)
-    context.user_data["profession"] = profession.name
+    if query and context.user_data:
+        profession = await Profession.objects.aget(professional_key=query.data)
+        context.user_data["profession"] = profession.name
     return await check_username(update, context)
 
 
 @debug_logger
-async def set_phone_number(update: Update, context: CallbackContext):
+async def set_phone_number(
+    update: Update, context: CallbackContext
+) -> Literal[States.SET_PHONE_NUMBER, States.PROFILE]:
     """Обработчик для ввода номера телефона."""
-    phone_number = update.message.text
-    if not await validation_phone_number(phone_number):
-        await update.message.reply_text(MESSAGE_INCORRECT_PHONE_NUMBER)
-        return States.SET_PHONE_NUMBER
-    context.user_data["contact"] = phone_number
-    await send_profile_form(update, context)
+    if update.message and update.message.text and context.user_data:
+        phone_number = update.message.text
+        if not await validation_phone_number(phone_number):
+            await update.message.reply_text(MESSAGE_INCORRECT_PHONE_NUMBER)
+            return States.SET_PHONE_NUMBER
+        context.user_data["contact"] = phone_number
+        await send_profile_form(update, context)
     return States.PROFILE
 
 
 @debug_logger
-async def profile(update: Update, context: CallbackContext):
+async def profile(
+    update: Update, context: CallbackContext
+) -> Optional[Literal[States.ROLE_CHOICE, States.START]]:
     """Обработчик для профиля."""
     query = update.callback_query
-    if query.data == "fill_again":
-        await query.edit_message_text(CHOOSE_ROLE_MESSAGE)
-        await query.edit_message_reply_markup(role_choice_keyboard_markup)
-        return States.ROLE_CHOICE
-    await to_create_user_in_db(update, context)
-    if await user_is_exist(query.from_user.id):
-        await query.edit_message_text(START_PAIR_SEARCH_MESSAGE)
-        await query.edit_message_reply_markup(
-            reply_markup=start_keyboard_markup
-        )
-        return States.START
-    else:
-        logger.error(
-            f"Пользователь {query.from_user} не сохранен в базе данных."
-        )
+    if query:
+        if query.data == "fill_again":
+            await query.edit_message_text(CHOOSE_ROLE_MESSAGE)
+            await query.edit_message_reply_markup(role_choice_keyboard_markup)
+            return States.ROLE_CHOICE
+        await to_create_user_in_db(update, context)
+        if await user_is_exist(query.from_user.id):
+            await query.edit_message_text(START_PAIR_SEARCH_MESSAGE)
+            await query.edit_message_reply_markup(
+                reply_markup=start_keyboard_markup
+            )
+            return States.START
+        else:
+            logger.error(
+                f"Пользователь {query.from_user} не сохранен в базе данных."
+            )
+    return None
 
 
-async def send_name_message(update: Update, context: CallbackContext):
+async def send_name_message(update: Update, context: CallbackContext) -> None:
     """Отправляет сообщение с именем."""
     query = update.callback_query
-    guessed_name = context.user_data.get(
-        "name", query.from_user.first_name if query else "unknown"
-    )
+    if context.user_data:
+        guessed_name = context.user_data.get(
+            "name", query.from_user.first_name if query else "unknown"
+        )
     if query:
         context.user_data["name"] = guessed_name
         await query.answer()
         await query.edit_message_text(GUESS_NAME_MESSAGE.format(guessed_name))
         await query.edit_message_reply_markup(guess_name_keyboard_markup)
-    else:
+    elif update.message:
         await update.message.reply_text(
             GUESS_NAME_MESSAGE.format(guessed_name),
             reply_markup=guess_name_keyboard_markup,
         )
 
 
-async def check_username(update: Update, context: CallbackContext):
+async def check_username(
+    update: Update, context: CallbackContext
+) -> Optional[Literal[States.PROFILE, States.SET_PHONE_NUMBER]]:
     """Проверяет наличие username."""
     query = update.callback_query
-    if not query.from_user.username:
-        await query.edit_message_text(USERNAME_NOT_FOUND_MESSAGE)
-        return States.SET_PHONE_NUMBER
-    await send_profile_form(update, context)
-    return States.PROFILE
+    if query:
+        if not query.from_user.username:
+            await query.edit_message_text(USERNAME_NOT_FOUND_MESSAGE)
+            return States.SET_PHONE_NUMBER
+        await send_profile_form(update, context)
+        return States.PROFILE
+    return None
 
 
-async def send_profile_form(update: Update, context: CallbackContext):
+async def send_profile_form(update: Update, context: CallbackContext) -> None:
     """Отправляет форму с именем или телефоном."""
     query = update.callback_query
-    profession = context.user_data["profession"]
-    name = context.user_data["name"]
+    if context.user_data:
+        profession = context.user_data["profession"]
+        name = context.user_data["name"]
     if query:
         context.user_data["contact"] = query.from_user.username
         await query.answer()
@@ -256,7 +311,7 @@ async def send_profile_form(update: Update, context: CallbackContext):
             )
         )
         await query.edit_message_reply_markup(profile_keyboard_markup)
-    else:
+    elif not query and update.message:
         await update.message.reply_text(
             PROFILE_MESSAGE_NO_USERNAME.format(
                 name, profession, context.user_data["contact"]
@@ -266,8 +321,11 @@ async def send_profile_form(update: Update, context: CallbackContext):
 
 
 async def send_both_users_message(
-    update: Update, context: CallbackContext, student, recruiter
-):
+    update: Update,
+    context: CallbackContext,
+    student: Student,
+    recruiter: Recruiter,
+) -> None:
     """Возвращает обоим пользователям информацию об их найденой паре."""
     guide_url = await get_form_url(FORM_KEYS["GUIDE"])
     student_profession = await Profession.objects.aget(
@@ -299,25 +357,30 @@ async def send_both_users_message(
     )
 
 
-async def to_create_user_in_db(update: Update, context: CallbackContext):
+async def to_create_user_in_db(
+    update: Update, context: CallbackContext
+) -> None:
     """Сохраняет пользователя в базе данных."""
     query = update.callback_query
-    user_data = {
-        "telegram_id": query.from_user.id,
-        "name": context.user_data["name"],
-        "surname": query.from_user.last_name,
-        "telegram_username": context.user_data["contact"],
-    }
-    try:
-        if context.user_data["role"] == "recruiter":
-            await Recruiter.objects.acreate(**user_data)
-        else:
-            profession = await Profession.objects.aget(
-                name=context.user_data["profession"]
-            )
-            await Student.objects.acreate(profession=profession, **user_data)
-    except Exception as error:
-        logger.error(f"Не удалось сохранить данные в таблицу: {error}")
+    if query and context.user_data:
+        user_data = {
+            "telegram_id": query.from_user.id,
+            "name": context.user_data["name"],
+            "surname": query.from_user.last_name,
+            "telegram_username": context.user_data["contact"],
+        }
+        try:
+            if context.user_data["role"] == "recruiter":
+                await Recruiter.objects.acreate(**user_data)
+            else:
+                profession = await Profession.objects.aget(
+                    name=context.user_data["profession"]
+                )
+                await Student.objects.acreate(
+                    profession=profession, **user_data
+                )
+        except Exception as error:
+            logger.error(f"Не удалось сохранить данные в таблицу: {error}")
 
 
 async def user_is_exist(user_id: int) -> bool:
@@ -347,25 +410,33 @@ async def get_active_pair(role: str, user_id: int) -> Union[CreatedPair, None]:
 
 
 @debug_logger
-async def calling_is_successful(update: Update, context: CallbackContext):
+async def calling_is_successful(
+    update: Update, context: CallbackContext
+) -> Literal[States.START]:
     """Возвращает пользователям сообщение об обратной связи."""
     query = update.callback_query
-    current_user = query.from_user
-    feedback_url = await get_form_url(FORM_KEYS["FEEDBACK"])
-    pair = await get_active_pair(context.user_data["role"], current_user.id)
-    await query.answer()
-    if pair:
-        await delete_pair(pair.student, pair.recruiter, query.data == "yes")
-    if query.data == "no":
-        communicate_url = await get_form_url(FORM_KEYS["FEEDBACK"])
-        await query.edit_message_text(
-            POST_CALL_MESSAGE.format(communicate_url)
+    if query and context.user_data:
+        feedback_url = await get_form_url(FORM_KEYS["FEEDBACK"])
+        pair = await get_active_pair(
+            context.user_data["role"], query.from_user.id
         )
-    elif context.user_data["role"] == "recruiter":
-        await query.edit_message_text(
-            POST_CALL_MESSAGE_FOR_RECRUITER.format(feedback_url)
+        await query.answer()
+        if pair:
+            await delete_pair(
+                pair.student, pair.recruiter, query.data == "yes"
+            )
+        if query.data == "no":
+            communicate_url = await get_form_url(FORM_KEYS["FEEDBACK"])
+            await query.edit_message_text(
+                POST_CALL_MESSAGE.format(communicate_url)
+            )
+        elif context.user_data["role"] == "recruiter":
+            await query.edit_message_text(
+                POST_CALL_MESSAGE_FOR_RECRUITER.format(feedback_url)
+            )
+        else:
+            await query.edit_message_text(POST_CALL_MESSAGE_FOR_STUDENT)
+        await query.edit_message_reply_markup(
+            reply_markup=start_keyboard_markup
         )
-    else:
-        await query.edit_message_text(POST_CALL_MESSAGE_FOR_STUDENT)
-    await query.edit_message_reply_markup(reply_markup=start_keyboard_markup)
     return States.START
--- src/bot/utils/message_senders.py
+++ src/bot/utils/message_senders.py
@@ -11,19 +11,21 @@ from core.config.logging import debug_logger
 
 
 @debug_logger
-async def send_is_pair_successful_message(context: CallbackContext):
+async def send_is_pair_successful_message(context: CallbackContext) -> None:
     """Отправляет сообщение состоялся ли звонок."""
-    await context.bot.send_message(
-        chat_id=context.job.user_id,
-        text=IS_PAIR_SUCCESSFUL_MESSAGE,
-        reply_markup=is_pair_successful_keyboard_markup,
-    )
+    if context.job:
+        await context.bot.send_message(
+            chat_id=context.job.user_id,
+            text=IS_PAIR_SUCCESSFUL_MESSAGE,
+            reply_markup=is_pair_successful_keyboard_markup,
+        )
 
 
 @debug_logger
-async def send_deleting_from_db_message(context: CallbackContext):
+async def send_deleting_from_db_message(context: CallbackContext) -> None:
     """Отправляет сообщение об удалении из базы данных."""
-    await context.bot.send_message(
-        chat_id=context.job.user_id,
-        text=USER_DELETE_MESSAGE.format(context.job.data),
-    )
+    if context.job:
+        await context.bot.send_message(
+            chat_id=context.job.user_id,
+            text=USER_DELETE_MESSAGE.format(context.job.data),
+        )
