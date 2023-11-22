cdf5cdd014b030fafcaf99f81ad6fb788806de7a
Roman Korzh <teenagehaze13@gmail.com>
2023-11-22 11:00:49 +0300

    changed nginx.conf & urls.py (3)

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,11 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import include, path
 
 urlpatterns = [
     path("admin_user/", include("admin_user.urls"), name="admin_user"),
-    path("admin/", admin.site.urls),
+    path("", lambda req: redirect("/admin/")),
+    path(" ", admin.site.urls),
 ]
 
 if settings.DEBUG:
