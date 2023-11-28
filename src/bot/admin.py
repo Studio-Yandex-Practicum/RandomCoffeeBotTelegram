97684716baeb027493f4ebbc350928f91b9c49a8
Roman Korzh <113125707+teehazee@users.noreply.github.com>
2023-11-28 15:06:40 +0300

    update&sort admin fields (#206)
    * update&sort admin fields
    
    * update&sort admin fields (2)
    
    * some fix
    
    ---------
    
    Co-authored-by: Konstantin Raikhert <raikhert13@gmail.com>

--- src/bot/admin.py
+++ src/bot/admin.py
@@ -60,11 +60,33 @@ class ItSpecialistAdmin(admin.ModelAdmin):
     """Управление моделью IT-специалиста."""
 
     form = ItSpecialistForm
-    list_display = (
-        "telegram_id",
+    fields = (
         "name",
         "surname",
+        "profession",
+        "has_pair",
+        "telegram_id",
         "telegram_username",
+        "last_login_date",
+        "registration_date",
+    )
+    readonly_fields = (
+        "telegram_id",
+        "telegram_username",
+        "last_login_date",
+        "registration_date",
+    )
+    list_display = (
+        "name",
+        "surname",
+        "telegram_id",
+        "telegram_username",
+        "profession",
+        "has_pair",
+        "last_login_date",
+        "registration_date",
+    )
+    list_filter = (
         "registration_date",
         "last_login_date",
         "profession",
@@ -84,15 +106,30 @@ class ItSpecialistAdmin(admin.ModelAdmin):
 class RecruiterAdmin(admin.ModelAdmin):
     """Управление моделью рекрутера."""
 
-    form = RecruiterForm
-    list_display = (
-        "telegram_id",
+    fields = (
         "name",
         "surname",
+        "has_pair",
+        "telegram_id",
         "telegram_username",
         "registration_date",
         "last_login_date",
+    )
+    readonly_fields = (
+        "telegram_id",
+        "telegram_username",
+        "last_login_date",
+        "registration_date",
+    )
+    form = RecruiterForm
+    list_display = (
+        "name",
+        "surname",
+        "telegram_id",
+        "telegram_username",
         "has_pair",
+        "last_login_date",
+        "registration_date",
     )
     list_filter = ("registration_date", "last_login_date", "has_pair")
     search_fields = ("telegram_id", "telegram_username")
